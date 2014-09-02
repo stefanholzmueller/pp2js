dc810a2d9c77aabb4610cb69fbb976e1160f62f1
Stefan Holzmüller <stefanholzmueller@github>
2014-09-02 13:41:22 +0200

    use RequireJS everywhere

--- src/pp2.js
+++ src/pp2.js
@@ -3,7 +3,8 @@
 
 define([
     'angular',
-    'check/controller'
+    'check/controller',
+    'ranged/ranged'
 ], function (angular, checkApp) {
     return angular.module('pp2', [ 'pp2.check' ]);
 });
