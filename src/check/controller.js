284bdd1b550e950ebc82f06b3f08d53b1fe018ee
Stefan Holzmüller <stefanholzmueller@github>
2014-09-05 20:31:29 +0200

    Updates

--- src/check/controller.js
+++ src/check/controller.js
@@ -11,9 +11,38 @@
             }
         };
 
-        $scope.$watch("check", function (newCheck) {
-            var partitioned = calculator.calculatePartitionedMemoized(newCheck);
-            $scope.pieData = [
+        $scope.tabs = [
+            {
+                template: "pie.tpl.html",
+                title: "Grafik"
+            },
+            {
+                template: "bar.tpl.html",
+                title: "Vergleich"
+            }
+        ];
+        $scope.currentTab = $scope.tabs[0];
+        $scope.switchTab = function (tab) {
+            $scope.currentTab = tab;
+        };
+
+        $scope.getPieData = function () {
+            function toCanvasjsPieDataPoints(partitioned) {
+                var partitions = partitioned[0].partitions;
+                var dataPoints = _.map(partitions, function (p) {
+                    return {
+                        x: p.quality,
+                        y: p.count,
+                        color: "#008000",
+                        toolTipContent: "gelungen mit {x}"
+                    };
+                });
+                dataPoints.push({ y: partitioned[1].count, color: "#bb0000", toolTipContent: "misslungen" });
+                return dataPoints;
+            }
+
+            var partitioned = calculator.calculatePartitionedMemoized(check);
+            return [
                 {
                     type: "pie",
                     startAngle: -90,
@@ -26,24 +55,36 @@
                     dataPoints: toCanvasjsPieDataPoints(partitioned)
                 }
             ];
+        };
 
-            var difficulties = _.uniq([12, 8, 4, 0, -4, -8, -12, newCheck.difficulty]).sort(function (a, b) {
+        $scope.getBarData = function () {
+            function difficultyToString(difficulty) {
+                if (difficulty > 0) {
+                    return "erschwert um " + difficulty;
+                } else if (difficulty < 0) {
+                    return "erleichtert um " + (-difficulty);
+                } else {
+                    return "nicht modifiziert";
+                }
+            }
+
+            var difficulties = _.uniq([12, 8, 4, 0, -4, -8, -12, $scope.check.difficulty]).sort(function (a, b) {
                 return a - b;
             });
             var checks = _.map(difficulties, function (difficulty) {
-                var check = _.merge(_.cloneDeep(newCheck), { difficulty: difficulty });
+                var check = _.merge(_.cloneDeep($scope.check), { difficulty: difficulty });
                 check.result = calculator.calculatePartitionedMemoized(check);
                 return check;
             });
             var dataPointsSuccess = _.map(checks, function (check) {
                 var failureCount = check.result[1].count;
-                return { y: 8000 - failureCount, label: difficultyToString(check.difficulty), color: (newCheck.difficulty === check.difficulty ? '#008000' : '#609060') };
+                return { y: 8000 - failureCount, label: difficultyToString(check.difficulty), color: ($scope.check.difficulty === check.difficulty ? '#008000' : '#609060') };
             });
             var dataPointsFailure = _.map(checks, function (check) {
-                return { y: check.result[1].count, label: difficultyToString(check.difficulty), color: (newCheck.difficulty === check.difficulty ? '#bb0000' : '#cc5050') };
+                return { y: check.result[1].count, label: difficultyToString(check.difficulty), color: ($scope.check.difficulty === check.difficulty ? '#bb0000' : '#cc5050') };
             });
 
-            $scope.barData = [
+            return [
                 {
                     type: "stackedBar100",
                     color: "#008000",
@@ -55,30 +96,6 @@
                     dataPoints: dataPointsFailure
                 }
             ];
-        }, true);
-
-        function toCanvasjsPieDataPoints(partitioned) {
-            var partitions = partitioned[0].partitions;
-            var dataPoints = _.map(partitions, function (p) {
-                return {
-                    x: p.quality,
-                    y: p.count,
-                    color: "#008000",
-                    toolTipContent: "gelungen mit {x}"
-                };
-            });
-            dataPoints.push({ y: partitioned[1].count, color: "#bb0000", toolTipContent: "misslungen" });
-            return dataPoints;
-        }
-
-        function difficultyToString(difficulty) {
-            if (difficulty > 0) {
-                return "erschwert um " + difficulty;
-            } else if (difficulty < 0) {
-                return "erleichtert um " + (-difficulty);
-            } else {
-                return "nicht modifiziert";
-            }
-        }
+        };
     }]);
 })(angular, _, calculator);
