63aea6ccaad2b56789932ecca00ef9c0abd8d657
Stefan Holzmüller <stefanholzmueller@github>
2014-09-07 17:05:52 +0200

    renaming of utils.js

--- src/filters.js
+++ src/filters.js
@@ -3,22 +3,6 @@
 
     var module = angular.module('pp2.utils', []);
 
-    module.factory('Util', function () {
-        return {
-            clone: function (obj) {
-                return JSON.parse(JSON.stringify(obj));
-            },
-            roll: function (pips) {
-                return Math.floor((Math.random() * pips) + 1);
-            },
-            sum: function (collection) {
-                return _.reduce(collection, function (acc, num) {
-                    return acc + num;
-                }, 0);
-            }
-        };
-    });
-
     module.filter('percentage', [
         '$filter', function (filter) {
             return function (number, fractionSize) {
