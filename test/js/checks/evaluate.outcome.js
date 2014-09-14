70221d36d1647dcc934b673fea61663d187793ba
Stefan Holzmüller <stefanholzmueller@github>
2014-09-14 10:30:16 +0200

    added more tests

--- test/js/checks/evaluate.outcome.js
+++ test/js/checks/evaluate.outcome.js
@@ -127,11 +127,89 @@
         equal(outcome.gap, 18);
     });
 
+    test("failure with easy check", function () {
+        var outcome = evaluator.evaluate(defaultOptions, defaultAttributes, 10, -10, [19, 19, 19]);
+        equal(outcome.success, false);
+        equal(outcome.gap, 1);
+    });
+
     test("success with difficulty > value", function () {
         var outcome = evaluator.evaluate(defaultOptions, defaultAttributes, 3, 8, [4, 3, 5]);
         equal(outcome.success, true);
         equal(outcome.quality, 1);
         equal(outcome.gap, 2);
+    });
+
+    test("no quality and gap with difficulty > value", function () {
+        var outcome = evaluator.evaluate(withoutMinimumQuality, defaultAttributes, 2, 9, [4, 5, 6]);
+        equal(outcome.success, true);
+        equal(outcome.quality, 0);
+        equal(outcome.gap, 0);
+    });
+
+    test("no quality but gap with difficulty > value", function () {
+        var outcome = evaluator.evaluate(withoutMinimumQuality, defaultAttributes, 2, 6, [4, 5, 6]);
+        equal(outcome.success, true);
+        equal(outcome.quality, 0);
+        equal(outcome.gap, 3);
+    });
+
+    test("full quality", function () {
+        var outcome = evaluator.evaluate(withoutMinimumQuality, defaultAttributes, 6, 0, [7, 3, 6]);
+        equal(outcome.success, true);
+        equal(outcome.quality, 6);
+        equal(outcome.gap, 10);
+    });
+
+    test("partial quality", function () {
+        var outcome = evaluator.evaluate(withoutMinimumQuality, defaultAttributes, 6, 0, [13, 3, 6]);
+        equal(outcome.success, true);
+        equal(outcome.quality, 4);
+        equal(outcome.gap, 4);
+    });
+
+    test("partial gap", function () {
+        var outcome = evaluator.evaluate(defaultOptions, defaultAttributes, 20, -10, [13, 12, 11]);
+        equal(outcome.success, true);
+        equal(outcome.quality, 20);
+        equal(outcome.gap, 28);
+    });
+
+    test("quality = gap", function () {
+        var outcome = evaluator.evaluate(defaultOptions, defaultAttributes, 20, -10, [16, 16, 16]);
+        equal(outcome.success, true);
+        equal(outcome.quality, 18);
+        equal(outcome.gap, 18);
+    });
+
+    test("spectacular success with full quality", function () {
+        var outcome = evaluator.evaluate(withoutMinimumQuality, defaultAttributes, 4, 0, [1, 1, 1]);
+        equal(outcome.success, true);
+        equal(outcome.quality, 4);
+    });
+
+    test("spectacular success with negative points", function () {
+        var outcome = evaluator.evaluate(withoutMinimumQuality, defaultAttributes, -2, -5, [1, 1, 1]);
+        equal(outcome.success, true);
+        equal(outcome.quality, 0);
+    });
+
+    test("spectacular success with negative points and minimum quality", function () {
+        var outcome = evaluator.evaluate(defaultOptions, defaultAttributes, -2, -5, [1, 1, 1]);
+        equal(outcome.success, true);
+        equal(outcome.quality, 1);
+    });
+
+    test("automatic success with negative difficulty", function () {
+        var outcome = evaluator.evaluate(defaultOptions, defaultAttributes, 3, -5, [1, 9, 1]);
+        equal(outcome.success, true);
+        equal(outcome.quality, 3);
+    });
+
+    test("automatic success with positive difficulty", function () {
+        var outcome = evaluator.evaluate(defaultOptions, defaultAttributes, 20, 10, [1, 1, 1]);
+        equal(outcome.success, true);
+        equal(outcome.quality, 20);
     });
 
 }());
